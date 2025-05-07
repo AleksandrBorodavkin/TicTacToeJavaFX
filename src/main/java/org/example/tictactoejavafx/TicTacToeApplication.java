bc214c1d1c94c087002294472f495930b9b660b9
Aleksandr Borodavkin <73429949+AleksandrBorodavkin@users.noreply.github.com>
2025-05-07 19:24:35 +0700

    Initial commit

--- src/main/java/org/example/tictactoejavafx/TicTacToeApplication.java
+++ src/main/java/org/example/tictactoejavafx/TicTacToeApplication.java
@@ -1,20 +1,28 @@
 package org.example.tictactoejavafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
+import java.util.Objects;
+
 public class TicTacToeApplication extends Application {
     @Override
-    public void start(Stage stage) {
-        GameController controller = new GameController();
-        GridPane board = controller.createBoard();
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                TicTacToeApplication.class.getResource("game-view.fxml")
+        );
 
-        Scene scene = new Scene(board, 300, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+
         stage.setTitle("Крестики-нолики");
+        stage.getIcons().add(new Image(Objects.requireNonNull(
+                TicTacToeApplication.class.getResource("icon.png")
+        ).toExternalForm()));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/tictactoejavafx/TicTacToeController.java
+++ src/main/java/org/example/tictactoejavafx/TicTacToeController.java
@@ -1,72 +1,94 @@
 package org.example.tictactoejavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class TicTacToeController {
-
-    @FXML private Button button00;
-    @FXML private Button button01;
-    @FXML private Button button02;
-    @FXML private Button button10;
-    @FXML private Button button11;
-    @FXML private Button button12;
-    @FXML private Button button20;
-    @FXML private Button button21;
-    @FXML private Button button22;
+    @FXML private GridPane boardGrid;
     @FXML private Label statusLabel;
 
-    private boolean isXTurn = true; // Начинает игрок X
-    private String[][] board = new String[3][3]; // Игровое поле
+    private String[][] board = new String[3][3];
+    private boolean isXTurn = true;
+
+
+
+
+    final Button[][] buttons = new Button[3][3];
+
 
     @FXML
-    private void handleButtonClick() {
-        Button clickedButton = (Button) event.getSource(); // Получаем нажатую кнопку
+    public void initialize() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                Button button = new Button();
+                button.setPrefSize(100, 100);
+                button.getStyleClass().addAll("btn", "btn-primary");
+                button.setOnAction(this::handleButtonClick);
+                boardGrid.add(button, col, row);
+                buttons[row][col] = button;
+                board[row][col] = null;
+            }
+        }
+        statusLabel.setText("Игрок X ходит");
+    }
+
+
+    @FXML
+    private void handleButtonClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
         int row = GridPane.getRowIndex(clickedButton);
         int col = GridPane.getColumnIndex(clickedButton);
+        String mark = isXTurn ? "X" : "O";
 
-        // Если ячейка пуста, то обновляем её
         if (board[row][col] == null) {
             board[row][col] = isXTurn ? "X" : "O";
             clickedButton.setText(board[row][col]);
+            clickedButton.setText(board[row][col]);
+            clickedButton.getStyleClass().add(mark.equals("X") ? "btn-info" : "btn-danger");;
 
-            // Проверяем на победу
             if (checkForWinner()) {
                 statusLabel.setText((isXTurn ? "X" : "O") + " выиграл!");
+                disableBoard();
                 return;
             }
 
-            // Переключаем ход
             isXTurn = !isXTurn;
             statusLabel.setText("Игрок " + (isXTurn ? "X" : "O") + " ходит");
         }
     }
 
+
+    @FXML
+    private void handleRestartGame() {
+        board = new String[3][3];
+        isXTurn = true;
+        statusLabel.setText("Игрок X ходит");
+
+        for (var node : boardGrid.getChildren()) {
+            if (node instanceof Button button) {
+                button.setText("");
+                button.getStyleClass().removeAll("btn-info", "btn-danger");
+                node.setDisable(false);
+            }
+        }
+    }
+
+    private void disableBoard() {
+        for (var node : boardGrid.getChildren()) {
+            node.setDisable(true);
+        }
+    }
+
     private boolean checkForWinner() {
-        // Проверка по строкам
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] != null && board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])) {
-                return true;
-            }
+            if (board[i][0] != null && board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])) return true;
+            if (board[0][i] != null && board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i])) return true;
         }
-
-        // Проверка по столбцам
-        for (int i = 0; i < 3; i++) {
-            if (board[0][i] != null && board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i])) {
-                return true;
-            }
-        }
-
-        // Проверка по диагоналям
-        if (board[0][0] != null && board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])) {
-            return true;
-        }
-        if (board[0][2] != null && board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0])) {
-            return true;
-        }
-
-        return false;
+        return board[0][0] != null && board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2]) ||
+                board[0][2] != null && board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0]);
     }
 }
+
